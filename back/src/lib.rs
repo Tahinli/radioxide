e26b0b9fd1872ee318e8bfec2ff78f58ef7d7d7c
Ahmet Kaan GÜMÜŞ <96421894+Tahinli@users.noreply.github.com>
2024-04-09 02:20:24 +0300

    refactor: :recycle: refactor ring with channel

--- back/src/lib.rs
+++ back/src/lib.rs
@@ -6,13 +6,14 @@ pub mod routing;
 pub mod streaming;
 pub mod utils;
 
+#[derive(Debug, Clone)]
+pub struct AppState {}
+
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Listener {
     ip: IpAddr,
     port: u16,
 }
-#[derive(Debug, Clone)]
-pub struct AppState {}
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 enum ServerStatus {
     Alive,
