5c1493f3366c00458783d90a0d01cf4437662571
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2025-09-07 11:06:39 +0300

    feat: :art: config

--- streamer/src/lib.rs
+++ streamer/src/lib.rs
@@ -2,3 +2,10 @@ pub mod recording;
 pub mod streaming;
 
 pub const BUFFER_LENGTH: usize = 1000000;
+
+pub struct Config {
+    pub address: String,
+    pub quality: u8,
+    pub latency: u16,
+    pub tls: bool,
+}
